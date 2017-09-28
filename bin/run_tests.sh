d8ffc2c4168f9c6eec0a4494b47aaa18e4188915
robnagler <github@q33.us>
2017-09-28 21:38:57 +0000

    pull submodules

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -4,6 +4,10 @@ set -e -x -u -o pipefail
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
+shopt -s nullglob
+if [[ -z $(echo deps/Catch/*) ]]; then
+    git submodule update --init --recursive
+fi
 docker pull radiasoft/beamsim:latest
 docker run --rm -u vagrant -v "${REPO_DIR}:/rslinac" radiasoft/beamsim /rslinac/bin/test_cpp.sh
 docker run --rm -v "${REPO_DIR}:/rslinac" radiasoft/beamsim /radia-run "$(id -u)" "$(id -g)" /rslinac/bin/test_python.sh
